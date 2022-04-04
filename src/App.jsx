61e227060b366d34890265ad98c730f62a3e711d
Md. Rony <rony15-10527@diu.edu.bd>
2022-04-04 15:32:07 +0600

    Header component added

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import './App.css';
+import Header from './components/Header/Header';
 function App() {
   return (
     <div className="App">
-     <h1 className='text-3xl font-bold underline'>This is Home</h1>
+     <Header></Header>
     </div>
   );
 }
