3d8d33b9b6cf8dd529dec156b108e4c731100bc9
Md. Rony <rony15-10527@diu.edu.bd>
2022-04-05 00:49:16 +0600

    custom hook added

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -6,11 +6,14 @@ const Home = () => {
    const [users, setUsers] = useReviews();
 
    return (
-     <div className="grid grid-cols-3 gap-4">
-       {users.slice(0, 3).map((user) => (
-         <User key={user.id} user={user}></User>
-       ))}
-     </div>
+      <>
+      <h1 className='text-center text-4xl m-4'>Customer Reviews(3)</h1>
+       <div className="grid grid-cols-3 gap-4">
+         {users.slice(0, 3).map((user) => (
+           <User key={user.id} user={user}></User>
+         ))}
+       </div>
+     </>
    );
 };
 
