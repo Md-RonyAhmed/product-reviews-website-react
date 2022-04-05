d0e52e573b27f6f740a8ffaf307676d95ebf19b4
Md. Rony <rony15-10527@diu.edu.bd>
2022-04-05 10:24:40 +0600

    NotFound page style updated

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -4,7 +4,7 @@ import Header from '../Header/Header';
 
 const About = () => {
    return (
-     <div className='bg-slate-200 h-full'>
+     <div className='bg-slate-200 h-screen'>
        <Header />
        <h1 className="text-5xl text-center m-6">About Us</h1>
        <div className='m-4 grid grid-cols-2 gap-3 pb-7'>
--- src/components/NotFound/NotFound.jsx
+++ src/components/NotFound/NotFound.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const NotFound = () => {
    return (
-      <div>
-         <h1>404</h1>
-      </div>
+     <div className="bg-slate-200 h-screen">
+       <div className="text-center py-10">
+         <h1 className="text-8xl font-bold text-red-700 mb-4">404</h1>
+         <p className="mb-4 pb-4">
+           <small className="text-purple-500 font-bold">Page not found</small>
+         </p>
+         <Link
+           to="/home"
+           className="bg-green-300 rounded m-4 p-2 hover:text-white hover:bg-black"
+         >
+           Back to Home
+         </Link>
+       </div>
+     </div>
    );
 };
 
