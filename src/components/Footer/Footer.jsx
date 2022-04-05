3fb0b1ee5a33bc05220ca8d1fa331d8dd2517d93
Md. Rony <rony15-10527@diu.edu.bd>
2022-04-05 11:26:39 +0600

    Footer component added

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+const Footer = () => {
+   return (
+      <div>
+         <p className='text-center bg-dark text-white p-2'><small>All rights reserve to Md.Rony</small></p>
+      </div>
+   );
+};
+
+export default Footer;
